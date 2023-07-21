763552bb55e35aacc7407d47452e23199ce28d55
Heba Omar <jtvonline@yahoo.com>
2023-07-22 02:42:27 +0300

    The big O notations of the time complexity of the Selection sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two ints
+ * @a: first int
+ * @b: second int
+ */
+
+void swap(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
+
+/**
+ * insertion_sort_list - sorts data
+ * @list: a pointer to a list
+ *
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+
+}
