7b2678af97a477826e4c0622c505db73b8666c73
Heba Omar <jtvonline@yahoo.com>
2023-07-24 22:50:16 +0300

    A function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,41 +15,60 @@ temp = *a;
 }
 
 /**
+ * quicksort_recursive - sorts array
+ * @array: the array to sort
+ * @size: size of the array to sort
+ * @left: low boundry of the array
+ * @right: high boundry of the array
+ *
+ */
+void quicksort_recursive(int *array, size_t size, int left, int right)
+{
+int part;
+
+if (right - left > 0)
+{
+part = partition(array, size, left, right);
+quicksort_recursive(array, size, left, part - 1);
+quicksort_recursive(array, size, part + 1, right);
+}
+}
+
+/**
  * partition - partition the array in place
  * @array: array to be partitioned
- * @low: low boundry of the array
- * @high: high boundry of the array
  * @size: size of the array
+ * @left: low boundry of the array
+ * @right: high boundry of the array
  *
  * Return: index of pivot
  */
-int partition(int *array, int low, int high, size_t size)
+int partition(int *array, size_t size, int left, int right)
 {
 
-int pivot = array[high];
-int mid = low - 1;
-int i;
+int *pivot, above, below;
+pivot = array + right;
 
-for (i = low; i <= high; i++)
+for (above = below = left; below < right; below++)
 {
-if (array[i] <= pivot)
+if (array[below] < *pivot)
 {
-mid++;
-if (mid != i)
+if (above < below)
 {
-swap(&array[mid], &array[i]);
+swap(array + below, array + above);
 print_array(array, size);
 }
+above++;
 }
 }
 
-if (mid + 1 != high)
+if (array[above] > *pivot)
 {
-swap(&array[mid + 1], &array[high]);
+swap_ints(array + above, pivot);
 print_array(array, size);
 }
 
-return (mid + 1);
+return (above);
 }
 
 /**
@@ -85,6 +104,6 @@ void quick_sort(int *array, size_t size)
 if (!array || size < 2)
 return;
 
-quickSort_recursion(array, 0, size - 1, size);
+quicksort_recursive(array, 0, size - 1, size);
 
 }
